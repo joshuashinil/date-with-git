985239ec75cab4088bf465cab0fcf0eebf421fd0
shinil joshua <shinil.joshua1994@gmail.com>
2020-07-16 14:56:18 +0530

    first commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+
+    int a,b,sum;
+    sum=a+b;
+    printf("enter the first number");
+    scanf("%d",&a);
+    printf("enter the second number");
+    scanf("%d",&b);
+
+    printf("sum of two numbers = ",&sum);
+    return EXIT_SUCCESS;
+
+}
